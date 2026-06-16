1b7dd7a3a4a2cd9dfb4d6d83e076a31f40fa6bf5
kota64453 <kota64453@gmail.com>
2026-06-16 17:49:21 +0530

    Initial commit

---
+++ src/main/java/model/Customer.java
@@ -0,0 +1,20 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class Customer {
+    private int id;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private long balance;
+
+}
